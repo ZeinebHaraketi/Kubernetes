484956fa54d9e0c33ab189e320c1ee2816565c29
Zeineb Haraketi <zeineb.haraketi@esprit.tn>
2024-02-03 22:42:10 +0100

    fixes

--- src/main/java/tn/esprit/spring/kaddem/services/IDepartementService.java
+++ src/main/java/tn/esprit/spring/kaddem/services/IDepartementService.java
@@ -1,15 +1,15 @@
 package tn.esprit.spring.kaddem.services;
 
 import tn.esprit.spring.kaddem.entities.Departement;
-
+import tn.esprit.spring.kaddem.dto.DepartementDTO;
 import java.util.List;
 
 public interface IDepartementService {
     public List<Departement> retrieveAllDepartements();
 
-    public DepartementDTO addDepartement(@RequestBody DepartementDTO departementDTO);
-	public DepartementDTO updateDepartement(@RequestBody DepartementDTO departementDTO);
-    
+    public DepartementDTO addDepartement( DepartementDTO departementDTO);
+	public DepartementDTO updateDepartement( DepartementDTO departementDTO);
+
     public  Departement retrieveDepartement (Integer idDepart);
 
     public  void deleteDepartement(Integer idDepartement);
