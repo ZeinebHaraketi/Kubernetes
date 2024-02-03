87ebd105caf8b1d8c4cb9a6595d5499ee83f0337
Zeineb Haraketi <zeineb.haraketi@esprit.tn>
2024-02-03 21:02:50 +0100

    UniversiteRestController

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -16,39 +16,32 @@ import java.util.Set;
 public class UniversiteRestController {
 	@Autowired
 	IUniversiteService universiteService;
-	// http://localhost:8089/Kaddem/universite/retrieve-all-universites
+
 	@GetMapping("/retrieve-all-universites")
 	public List<Universite> getUniversites() {
-		List<Universite> listUniversites = universiteService.retrieveAllUniversites();
-		return listUniversites;
+		return universiteService.retrieveAllUniversites();
 	}
-	// http://localhost:8089/Kaddem/universite/retrieve-universite/8
+
 	@GetMapping("/retrieve-universite/{universite-id}")
 	public Universite retrieveUniversite(@PathVariable("universite-id") Integer universiteId) {
 		return universiteService.retrieveUniversite(universiteId);
 	}
 
-	// http://localhost:8089/Kaddem/universite/add-universite
 	@PostMapping("/add-universite")
 	public Universite addUniversite(@RequestBody Universite u) {
-		Universite universite = universiteService.addUniversite(u);
-		return universite;
+		return universiteService.addUniversite(u);
 	}
 
-	// http://localhost:8089/Kaddem/universite/remove-universite/1
 	@DeleteMapping("/remove-universite/{universite-id}")
 	public void removeUniversite(@PathVariable("universite-id") Integer universiteId) {
 		universiteService.deleteUniversite(universiteId);
 	}
 
-	// http://localhost:8089/Kaddem/universite/update-universite
 	@PutMapping("/update-universite")
 	public Universite updateUniversite(@RequestBody Universite u) {
-		Universite u1= universiteService.updateUniversite(u);
-		return u1;
+		return universiteService.updateUniversite(u);
 	}
 
-	//@PutMapping("/affecter-etudiant-departement")
 	@PutMapping(value="/affecter-universite-departement/{universiteId}/{departementId}")
 	public void affectertUniversiteToDepartement(@PathVariable("universiteId") Integer universiteId, @PathVariable("departementId")Integer departementId){
 		universiteService.assignUniversiteToDepartement(universiteId, departementId);
