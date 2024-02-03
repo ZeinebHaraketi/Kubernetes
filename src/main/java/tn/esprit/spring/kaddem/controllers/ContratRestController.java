22ec87ea2798afe4bb9ed051e6ee42da6bfc4872
Zeineb Haraketi <zeineb.haraketi@esprit.tn>
2024-02-03 20:50:30 +0100

    ContratRestController

--- src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
@@ -1,12 +1,10 @@
 package tn.esprit.spring.kaddem.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Contrat;
-import tn.esprit.spring.kaddem.services.ContratServiceImpl;
 import tn.esprit.spring.kaddem.services.IContratService;
 
 import java.util.Date;
@@ -17,46 +15,37 @@ import java.util.List;
 @RequestMapping("/contrat")
 public class ContratRestController {
 	IContratService contratService;
-	// http://localhost:8089/Kaddem/contrat/retrieve-all-contrats
+
 	@GetMapping("/retrieve-all-contrats")
-	public List<Contrat> getContrats() {
-		List<Contrat> listContrats = contratService.retrieveAllContrats();
-		return listContrats;
-	}
-	// http://localhost:8089/Kaddem/contrat/retrieve-contrat/8
+public List<Contrat> getContrats() {
+    return contratService.retrieveAllContrats();
+}
+
 	@GetMapping("/retrieve-contrat/{contrat-id}")
 	public Contrat retrieveContrat(@PathVariable("contrat-id") Integer contratId) {
 		return contratService.retrieveContrat(contratId);
 	}
 
-	// http://localhost:8089/Kaddem/econtrat/add-contrat
 	@PostMapping("/add-contrat")
 	public Contrat addContrat(@RequestBody Contrat c) {
-		Contrat contrat = contratService.addContrat(c);
-		return contrat;
+    	return contratService.addContrat(c);
 	}
 
-	// http://localhost:8089/Kaddem/contrat/remove-contrat/1
+
 	@DeleteMapping("/remove-contrat/{contrat-id}")
 	public void removeContrat(@PathVariable("contrat-id") Integer contratId) {
 		contratService.removeContrat(contratId);
 	}
 
-	// http://localhost:8089/Kaddem/contrat/update-contrat
 	@PutMapping("/update-contrat")
 	public Contrat updateContrat(@RequestBody Contrat c) {
-		Contrat contrat= contratService.updateContrat(c);
-		return contrat;
+    	return contratService.updateContrat(c);
 	}
 
-		/*@PutMapping(value = "/assignContratToEtudiant/{ce}/{nomE}/{prenomE}")
-		public Contrat assignContratToEtudiant (Contrat ce, String nomE, String prenomE){
-		return 	(contratService.affectContratToEtudiant(ce, nomE, prenomE));
-		}*/
+
 
 	@PutMapping(value = "/assignContratToEtudiant/{idContrat}/{nomE}/{prenomE}")
 	public Contrat assignContratToEtudiant (Integer idContrat, String nomE, String prenomE){
-	//	Contrat c= contratService.affectContratToEtudiant()
 		return 	(contratService.affectContratToEtudiant(idContrat, nomE, prenomE));
 	}
 
@@ -68,11 +57,9 @@ public class ContratRestController {
 			return contratService.nbContratsValides(startDate, endDate);
 		}
 
-    //Only no-arg methods may be annotated with @Scheduled
     @Scheduled(cron="0 0 13 * * *")//(cron="0 0 13 * * ?")(fixedRate =21600)
 	@PutMapping(value = "/majStatusContrat")
 	public void majStatusContrat (){
-		//return 	(contratService.affectContratToEtudiant(ce, nomE, prenomE));
 		contratService.retrieveAndUpdateStatusContrat();
 
 	}
