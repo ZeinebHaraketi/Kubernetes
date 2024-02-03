af799b23090f66ff368c43bc282c12a10ee139ac
Zeineb Haraketi <zeineb.haraketi@esprit.tn>
2024-02-03 22:47:06 +0100

    fixes

--- src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import tn.esprit.spring.kaddem.entities.Departement;
-import tn.esprit.spring.kaddem.dto.DepartementDTO;
-
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 
 import java.util.List;
@@ -21,40 +19,17 @@ public class DepartementServiceImpl implements IDepartementService{
 		return (List<Departement>) departementRepository.findAll();
 	}
 
-public DepartementDTO addDepartement( DepartementDTO departementDTO) {
-    Departement departement = new Departement();
-    departement.setNomDepart(departementDTO.getNomDepart());
+	public Departement addDepartement (Departement d){
+		return departementRepository.save(d);
+	}
 
-    departement = departementRepository.save(departement);
+	public   Departement updateDepartement (Departement d){
+		return departementRepository.save(d);
+	}
 
-    DepartementDTO responseDTO = new DepartementDTO();
-    responseDTO.setIdDepart(departement.getIdDepart());
-    responseDTO.setNomDepart(departement.getNomDepart());
-
-    return responseDTO;
-}
-
-
-	public DepartementDTO updateDepartement( DepartementDTO departementDTO) {
-    Departement departementToUpdate = departementRepository.findById(departementDTO.getIdDepart())
-            .orElseThrow(() -> new EntityNotFoundException("Departement not found with ID: " + departementDTO.getIdDepart()));
-
-    departementToUpdate.setNomDepart(departementDTO.getNomDepart());
-
-    Departement updatedDepartement = departementRepository.save(departementToUpdate);
-
-    DepartementDTO responseDTO = new DepartementDTO();
-    responseDTO.setIdDepart(updatedDepartement.getIdDepart());
-    responseDTO.setNomDepart(updatedDepartement.getNomDepart());
-
-    return responseDTO;
-}
-
-
-	public Departement retrieveDepartement(Integer idDepart) {
-    return departementRepository.findById(idDepart).orElse(null);
-}
-
+	public  Departement retrieveDepartement (Integer idDepart){
+		return departementRepository.findById(idDepart).get();
+	}
 	public  void deleteDepartement(Integer idDepartement){
 		Departement d=retrieveDepartement(idDepartement);
 		departementRepository.delete(d);
--- src/main/java/tn/esprit/spring/kaddem/services/IDepartementService.java
+++ src/main/java/tn/esprit/spring/kaddem/services/IDepartementService.java
@@ -1,14 +1,15 @@
 package tn.esprit.spring.kaddem.services;
 
 import tn.esprit.spring.kaddem.entities.Departement;
-import tn.esprit.spring.kaddem.dto.DepartementDTO;
+
 import java.util.List;
 
 public interface IDepartementService {
     public List<Departement> retrieveAllDepartements();
 
-    public DepartementDTO addDepartement( DepartementDTO departementDTO);
-	public DepartementDTO updateDepartement( DepartementDTO departementDTO);
+    public Departement addDepartement (Departement d);
+
+    public   Departement updateDepartement (Departement d);
 
     public  Departement retrieveDepartement (Integer idDepart);
 
